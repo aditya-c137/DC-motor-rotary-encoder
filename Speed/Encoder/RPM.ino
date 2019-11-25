b2701625160ee7045f0fc655301a0df4212a3213
Aditya Borse <57841103+aditya-c137@users.noreply.github.com>
2019-11-25 16:28:02 +0530

    Update RPM.ino

--- Speed/Encoder/RPM.ino
+++ Speed/Encoder/RPM.ino
@@ -1,7 +1,33 @@
-void setup(){
+#define RPMcoeff 37500
 
+volatile long _counter;
+double current_counter = 0,prev_counter = 0;
+long why_so_long = 0,why_so_much,why_so_early;
+double delta = 0,RPM = 0;
+
+
+void setup() {
+  pinMode(2,INPUT_PULLUP);
+  pinMode(3,INPUT_PULLUP);
+  Serial.begin(9600);
+  attachInterrupt(digitalPinToInterrupt(3),eh,CHANGE);
+  attachInterrupt(digitalPinToInterrupt(2),eh,CHANGE);
 }
 
-void loop(){
+void loop() {
+  why_so_early = micros();
+  if((why_so_early-why_so_long) >100000){
+    why_so_much = micros() - why_so_long;
+    why_so_long = micros();
+    delta = _counter - prev_counter;
+    prev_counter = _counter;
+    RPM = double(delta * RPMcoeff)/double(why_so_much);
+    Serial.print(delta);
+    Serial.print("\t");
+    Serial.println(RPM);
+  }
+}
 
+void eh(){
+  _counter++;
 }
